0221c70277609071e2442c01c41db9d7eab89986
brocode <kelimbth@gmail.com>
2024-06-06 15:19:07 +0300

    created a login screen

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/constants/colors.dart';
+import 'package:whatsapp_clone/screens/login/components/body.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  static String routeName = '/login';
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final mobileNumberController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kbackgroundColor,
+        centerTitle: true,
+        title: const Text('Enter your mobile number'),
+      ),
+      body: LoginScreenBody(mobileNumberController: mobileNumberController),
+    );
+  }
+}
