e3a9fc6839ab32e0037474421e11293d816f9c3f
brocode <kelimbth@gmail.com>
2024-06-06 14:58:15 +0300

    added routeName for LoginScreen()

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/screens/login/login_screen.dart';
 import 'package:whatsapp_clone/screens/onboarding_screen/onboarding_screen.dart';
 
 
 final Map<String, WidgetBuilder> routes = {
   OnboardingScreen.routeName: (context) => const OnboardingScreen(),
+  LoginScreen.routeName: (context) => const LoginScreen(),
 } ;
