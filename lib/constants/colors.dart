1703b6b8957b787f338cb3f2c5fb5268634c6a41
brocode <kelimbth@gmail.com>
2024-06-06 13:44:57 +0300

    added icon color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF00A783);
 const kbackgroundColor = Color(0xFF131C21);
 const kTextColor = Color(0xFFF1F1F2);
+const kIconColor = Color(0xFFF1F1F2);
 const kAppBarColor = Color(0xFF1F2C34);
 const kmessageBubbleColor = Color(0x00056062);
 const ksenderMessageBubbleColor = Color(0xFF252D31);
