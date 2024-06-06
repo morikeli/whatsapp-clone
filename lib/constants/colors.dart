14a594eb9b916bfc107bfe8f588ccda20275dad7
brocode <kelimbth@gmail.com>
2024-06-06 13:07:19 +0300

    first commit 🚀: colors used in the app

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+const kPrimaryColor = Color(0xFF00A783);
+const kbackgroundColor = Color(0xFF131C21);
+const kTextColor = Color(0xFFF1F1F2);
+const kAppBarColor = Color(0xFF1F2C34);
+const kmessageBubbleColor = Color(0x00056062);
+const ksenderMessageBubbleColor = Color(0xFF252D31);
+const kTabColor = Color(0xFF00A783);
+const ksearchBarColor = Color(0xFF323739);
+const kDividerColor = Color(0xFF252D32);
+const kchatBarMessage = Color(0xFF1E2428);
+const kmobileChatBoxColor = Color(0xFF1F2C34);
