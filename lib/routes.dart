7a2b708e2a113445caa3637dd57eeffa3e70b94a
brocode <kelimbth@gmail.com>
2024-06-06 13:30:59 +0300

    first commit 🚀

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/screens/onboarding_screen/onboarding_screen.dart';
+
+
+final Map<String, WidgetBuilder> routes = {
+  OnboardingScreen.routeName: (context) => const OnboardingScreen(),
+} ;
