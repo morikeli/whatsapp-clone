0b4639d69dbb28012080a2f44b49b87726347547
brocode <kelimbth@gmail.com>
2024-06-07 12:06:15 +0300

    appended route name for signup screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/screens/login/login_screen.dart';
 import 'package:whatsapp_clone/screens/onboarding_screen/onboarding_screen.dart';
 import 'package:whatsapp_clone/screens/otp/otp_screen.dart';
+import 'package:whatsapp_clone/screens/signup/signup_screen.dart';
 
 
 final Map<String, WidgetBuilder> routes = {
   OnboardingScreen.routeName: (context) => const OnboardingScreen(),
   LoginScreen.routeName: (context) => const LoginScreen(),
   OTPScreen.routeName:(context) => const OTPScreen(),
+  SignupScreen.routeName: (context) => const SignupScreen(),
 } ;
