6fb89b5844c1771996e09f39d75709408e54d05a
brocode <kelimbth@gmail.com>
2024-06-07 11:47:57 +0300

    created widget to enter OTP code

---
+++ lib/screens/otp/components/otp_input_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/constants/colors.dart';
+
+
+class OtpInputField extends StatelessWidget {
+  const OtpInputField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * .5,
+      child: TextField(
+        decoration: const InputDecoration(
+          hintText: '------',
+          hintStyle: TextStyle(fontSize: 30.0, color: kTextColor)
+        ),
+        keyboardType: TextInputType.number,
+        style: const TextStyle(color: kTextColor),
+        textAlign: TextAlign.center,
+        onChanged: (value) {},
+      ),
+    );
+  }
+}
