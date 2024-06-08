16eead61a4f49e091604adef026f32ee7f706c9c
brocode <kelimbth@gmail.com>
2024-06-08 09:46:12 +0300

    created appbar for the homescreen

---
+++ lib/screens/homepage/components/appbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+AppBar homepageHeader() {
+  return AppBar(
+    automaticallyImplyLeading: false,
+    title: const Text(
+      'WhatsApp',
+      style: TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    actions: [
+      IconButton(
+        onPressed: () {}, 
+        icon: const Icon(Icons.camera_alt_outlined),
+      ),
+      IconButton(
+        onPressed: () {}, 
+        icon: const Icon(Icons.search_outlined),
+      ),
+      IconButton(
+        onPressed: () {}, 
+        icon: const Icon(Icons.more_vert),
+      ),
+    ],
+  );
+}
