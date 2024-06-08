ae74c93c769dbdb142c8501a3412f7b8cf69ce2e
brocode <kelimbth@gmail.com>
2024-06-08 10:07:05 +0300

    created a bottom navigation bar

---
+++ lib/screens/homepage/components/widgets/bottom_navbar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/screens/homepage/components/widgets/icons/chat_icon.dart';
+import 'package:whatsapp_clone/screens/homepage/components/widgets/icons/updates_icon.dart';
+
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          activeIcon: ChatsIcon(icon: Icons.message),
+          icon: ChatsIcon(icon: Icons.message_outlined),
+          label: 'Chats'
+        ),
+        BottomNavigationBarItem(
+          activeIcon: UpdatesIconWidget(icon: Icons.notifications),
+          icon: UpdatesIconWidget(icon: Icons.notifications_outlined),
+          label: 'Updates'
+        ),
+        BottomNavigationBarItem(
+          activeIcon: Icon(Icons.people),
+          icon: Icon(Icons.people_outline),
+          label: 'Communities'
+        ),
+        BottomNavigationBarItem(
+          activeIcon: Icon(Icons.phone),
+          icon: Icon(Icons.phone_outlined),
+          label: 'Calls'
+        ),
+        
+      ],
+    );
+  }
+}
