6b0d9136efe7c90bcc77b2985e74e6b144a6a415
brocode <kelimbth@gmail.com>
2024-06-09 12:09:56 +0300

    created a ListTile to display recent calls

---
+++ lib/screens/calls/components/widgets/call_records_tile.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/constants/colors.dart';
+
+
+class CallRecordTile extends StatelessWidget {
+  final String username;
+  final String profilePicture;
+  final String timeStamp;
+  final IconData recordIcon;
+  final Color iconColor;
+  final Color usernameColor;
+
+
+  const CallRecordTile({
+    super.key,
+    required this.username,
+    required this.profilePicture,
+    required this.timeStamp,
+    required this.recordIcon,
+    this.iconColor = Colors.red,
+    this.usernameColor = Colors.red,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: kPrimaryColor,
+        backgroundImage: AssetImage(profilePicture),
+        radius: MediaQuery.of(context).size.width * .07,
+      ),
+      title: Text(
+        username,
+        style: TextStyle(color: usernameColor),
+      ),
+      subtitle: Row(
+        children: [
+          Icon(recordIcon, color: iconColor, size: 18.0),
+          const SizedBox(width: 6.0),
+          Text(
+            timeStamp,
+            style: const TextStyle(color: kTextDarkColor,),
+          ),
+        ],
+      ),
+      trailing: const Icon(Icons.phone_outlined, color: kIconColor,),
+    );
+  }
+}
