d9cc094867ff05011b097670e66ded5ce497cc88
brocode <kelimbth@gmail.com>
2024-06-09 12:20:02 +0300

    deleted 'bottom_navbar.dart' module and routeName

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/screens/homepage/components/appbar.dart';
 import 'package:whatsapp_clone/screens/homepage/components/body.dart';
 import 'package:whatsapp_clone/screens/homepage/components/widgets/new_message.dart';
-import 'package:whatsapp_clone/screens/homepage/components/widgets/bottom_navbar.dart';
 
 
 class Homepage extends StatelessWidget {
-  static String routeName = '/homepage'; 
   const Homepage({super.key});
 
   @override
