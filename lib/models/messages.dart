e3a46a0c7b0ec7c962f9a7c7ab2671f172298737
brocode <kelimbth@gmail.com>
2024-06-12 13:21:44 +0300

    dummy data for chats - sent and received messages in chat screen

---
+++ lib/models/messages.dart
@@ -0,0 +1,53 @@
+List<Map<String, dynamic>> inboxMessages = [
+  {
+    "message": "Hey handsome 🥰🥰",
+    "timeStamp": "20:00",
+    "isSender": false,
+  },
+  {
+    "message": "Hi Sarah. How was your day? 😏",
+    "timeStamp": "20:02",
+    "isSender": true,
+  },
+  {
+    "message": "I had such a hectic day ... 😩😩😓",
+    "timeStamp": "20:03",
+    "isSender": false,
+  },
+  {
+    "message": "So sorry. What tasks were you given today?",
+    "timeStamp": "20:06",
+    "isSender": true,
+  },
+  {
+    "message": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, numquam omnis aspernatur illo tenetur beatae labore impedit eveniet, explicabo voluptates eaque sint qui odit error fugiat ex sunt asperiores quae.",
+    "timeStamp": "20:15",
+    "isSender": false,
+  },
+  {
+    "message": "😂😂🥵",
+    "timeStamp": "20:20",
+    "isSender": true,
+  },
+  {
+    "message": "😒😒😒",
+    "timeStamp": "20:20",
+    "isSender": false,
+  },
+  {
+    "message": "😒😒😒😒",
+    "timeStamp": "20:20",
+    "isSender": false,
+  },
+  {
+    "message": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, numquam omnis aspernatur illo tenetur beatae labore impedit eveniet, explicabo voluptates eaque sint qui odit error fugiat ex sunt asperiores quae.",
+    "timeStamp": "20:25",
+    "isSender": true,
+  },
+  {
+    "message": "Hello daddzzzyy! 😍😍",
+    "timeStamp": "20:00",
+    "isSender": false,
+  },
+  
+];
