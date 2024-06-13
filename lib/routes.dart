29d741284e31d09695daac09724b6720799fb12e
brocode <kelimbth@gmail.com>
2024-06-13 10:40:00 +0300

    appended new route names

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,8 @@ import 'package:whatsapp_clone/screens/chats/chat_screen.dart';
 import 'package:whatsapp_clone/screens/login/login_screen.dart';
 import 'package:whatsapp_clone/screens/onboarding_screen/onboarding_screen.dart';
 import 'package:whatsapp_clone/screens/otp/otp_screen.dart';
+import 'package:whatsapp_clone/screens/profile/profile_screen.dart';
+import 'package:whatsapp_clone/screens/settings/settings_screen.dart';
 import 'package:whatsapp_clone/screens/signup/signup_screen.dart';
 
 
@@ -14,4 +16,6 @@ final Map<String, WidgetBuilder> routes = {
   SignupScreen.routeName: (context) => const SignupScreen(),
   HomeScreen.routeName: (context) => const HomeScreen(),
   ChatScreen.routeName: (context) => const ChatScreen(),
+  SettingsScreen.routeName: (context) => const SettingsScreen(),
+  ProfileScreen.routeName: (context) => const ProfileScreen(),
 } ;
