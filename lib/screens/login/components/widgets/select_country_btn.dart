e106768bc4bd977d7459ca2811c87b2da4d61373
brocode <kelimbth@gmail.com>
2024-06-15 09:32:28 +0300

    made some changes 1.) changed SelectCountryButton() to a stateful widget 2.) added 'selectCountry' in the class constructor (line 9) 3.) initialized attr. 'selectCountry' (line 5) 4.) updated onPressed to use the value passed in selectCountry (line 20)

--- lib/screens/login/components/widgets/select_country_btn.dart
+++ lib/screens/login/components/widgets/select_country_btn.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 
-class SelectCountryButton extends StatelessWidget {
+class SelectCountryButton extends StatefulWidget {
+  final void Function() selectCountry;
+  
   const SelectCountryButton({
     super.key,
+    required this.selectCountry
   });
 
   @override
+  State<SelectCountryButton> createState() => _SelectCountryButtonState();
+}
+
+class _SelectCountryButtonState extends State<SelectCountryButton> {
+  @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {}, 
+      onPressed: widget.selectCountry, 
       child: const Text(
         'Pick a country',
       ),
