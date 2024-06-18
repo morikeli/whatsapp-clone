7aabd52ab1106789057b144cba2e3034d4019413
brocode <kelimbth@gmail.com>
2024-06-18 10:07:49 +0300

    created a widget to display the header section for suggested channels

---
+++ lib/screens/updates/components/widgets/channels/suggested_channels_header.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/constants/colors.dart';
+
+
+class SuggestedChannelsHeader extends StatelessWidget {
+  const SuggestedChannelsHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Row(
+        children: [
+          const Text(
+            'Suggested channels',
+            style: TextStyle(
+              color: kTextColor,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const Spacer(),
+          GestureDetector(
+            onTap: () {},
+            child: const Text(
+              'See all',
+              style: TextStyle(color: kPrimaryColor),
+            ),
+          ),
+          const Icon(Icons.keyboard_arrow_right_outlined, color: kPrimaryColor)
+        ],
+      ),
+    );
+  }
+}
