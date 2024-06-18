74d3e435c0ba5f7cbfe339702d0a684db780dad5
brocode <kelimbth@gmail.com>
2024-06-18 10:13:24 +0300

    updated title

--- lib/screens/updates/components/widgets/channels/suggested_channels_header.dart
+++ lib/screens/updates/components/widgets/channels/suggested_channels_header.dart
@@ -14,7 +14,7 @@ class SuggestedChannelsHeader extends StatelessWidget {
       child: Row(
         children: [
           const Text(
-            'Suggested channels',
+            'Find channels',
             style: TextStyle(
               color: kTextColor,
               fontSize: 16.0,
