95d1fb572c5ae833aa704c2b3922d8cda894d810
brocode <kelimbth@gmail.com>
2024-06-19 10:18:14 +0300

    added new items in list uploadedStatus

--- lib/models/status.dart
+++ lib/models/status.dart
@@ -17,5 +17,15 @@ List<Status> uploadedStatus = [
   const Status(profilePicture: 'assets/img/dps/2.jpg', username: 'Sharon'),
   const Status(profilePicture: 'assets/img/dps/4.jpg', username: 'John Doe'),
   const Status(profilePicture: 'assets/img/dps/9.jpg', username: 'Bazuu'),
+  const Status(profilePicture: 'assets/img/dps/13.jpg', username: 'Boss'),
+  const Status(profilePicture: 'assets/img/dps/14.jpg', username: 'Kelvin'),
+  const Status(profilePicture: 'assets/img/dps/15.jpg', username: 'Joan'),
+  const Status(profilePicture: 'assets/img/dps/5.jpg', username: 'Irene'),
+  const Status(profilePicture: 'assets/img/dps/16.jpg', username: 'Priscilla'),
+  const Status(profilePicture: 'assets/img/dps/17.jpg', username: 'Winnie'),
+  const Status(profilePicture: 'assets/img/dps/18.jpg', username: 'Rose'),
+  const Status(profilePicture: 'assets/img/dps/19.jpg', username: 'Walter'),
+  const Status(profilePicture: 'assets/img/dps/20.jpg', username: 'Ronald'),
+  const Status(profilePicture: 'assets/img/dps/21.jpg', username: 'Mike'),
 
 ];
